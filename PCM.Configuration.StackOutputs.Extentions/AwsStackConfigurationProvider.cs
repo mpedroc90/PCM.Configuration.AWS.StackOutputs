3914dcb256682b12a6525d146000bd7e2619b3ca
mpedroc90 <mpedroc90@gmail.com>
2018-08-30 16:02:44 -0400

    Add project files.

---
+++ PCM.Configuration.StackOutputs.Extentions/AwsStackConfigurationProvider.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Amazon;
+using Amazon.CloudFormation;
+using Amazon.CloudFormation.Model;
+using Amazon.Runtime;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Primitives;
+
+namespace GDExpress.Tools.Configuration.Extentions.StackOutputs
+{
+    public class AwsStackConfigurationProvider : ConfigurationProvider
+    {
+        private List<Output> _outputs;
+
+        private readonly string _stack;
+        private readonly string _accessKey;
+        private readonly string _secretKey;
+        private readonly string _region;
+
+        public AwsStackConfigurationProvider(string stack, string accessKey, string secretKey, string region)
+        {
+            _stack = stack;
+            _accessKey = accessKey;
+            _secretKey = secretKey;
+            _region = region;
+        }
+
+
+      
+        public override bool TryGet(string key, out string value)
+        {
+            var aux =_outputs.FirstOrDefault(t => t.ExportName == key);
+
+            value = null;
+            if (aux == null)
+                return false;
+            value = aux.OutputValue;
+            return true;
+        }
+
+     
+
+        public override void Load()
+        {
+            var client = new AmazonCloudFormationClient(new BasicAWSCredentials(_accessKey, _secretKey), RegionEndpoint.GetBySystemName(_region));
+
+            var request = new DescribeStacksRequest()
+            {
+                StackName = _stack
+            };
+
+            var response = client.DescribeStacksAsync(request).GetAwaiter().GetResult();
+            _outputs = response.Stacks.FirstOrDefault()?.Outputs.Where(t=> !string.IsNullOrEmpty(t.ExportName)).ToList();
+
+        }
+
+    }
+}
---
+++ PCM.Configuration.StackOutputs.Extentions/ConfigurationExtentions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace GDExpress.Tools.Configuration.Extentions.StackOutputs
+{
+    public static class ConfigurationExtentions
+
+    {
+        public static IConfigurationBuilder AddStackConfig(this IConfigurationBuilder builder, string stack,string accessKey, string secretKey, string region)
+        {
+            builder.Add(new AwsStackConfigurationSource(stack,accessKey, secretKey, region));
+            return builder;
+
+        }
+
+        public static IConfigurationBuilder AddStackConfig(this IConfigurationBuilder builder, string stack)
+        {
+            builder.Add(new AwsStackConfigurationSource(stack));
+            return builder;
+
+        }
+
+
+        public static IConfigurationBuilder AddStackConfig(this IConfigurationBuilder builder, string stack, string profile)
+        {
+            builder.Add(new AwsStackConfigurationSource(stack));
+            return builder;
+
+        }
+
+    }
+}
